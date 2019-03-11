c33dbabaa3b6065a2bbbc0fd2ef8c9fe702abdd9
TedYim <yan3579@gmail.com>
2019-03-11 22:47:17 +0800

    增加排序例子

--- src/main/java/com/example/demo/sort_algorithm/Selection.java
+++ src/main/java/com/example/demo/sort_algorithm/Selection.java
@@ -7,10 +7,10 @@ public class Selection {
 
     public static void selectSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            int tmp = arr[i];
+            int tmp = arr[i];//待选择的元素
             for (int j = i + 1; j < arr.length; j++) {
-                int tmp1 = arr[j];
-                if (tmp1 < tmp) {
+                int tmp1 = arr[j];//待比较的元素
+                if (tmp1 < tmp) { //如果
                     tmp = tmp1;
                     exchange(arr, i, j);
                 }
@@ -26,9 +26,21 @@ public class Selection {
 
     public static void main(String[] args) {
         int[] arr = {3, 54, 6, 4, 43, 5, 6, 6, 6, 5, 5, 9};
-        Selection.selectSort(arr);
+        sort(arr);
         for (int i : arr) {
             System.out.println(i);
+        }
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int tmp = i;//暂时的最小值的坐标
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[tmp] > arr[j]) {
+                    exchange(arr, tmp, j);//交换一次 , i位置的元素就是当前比较的最小值 , 当全部交换完之后 , 就是最小的元素
+                }
+
+            }
         }
     }
 }
