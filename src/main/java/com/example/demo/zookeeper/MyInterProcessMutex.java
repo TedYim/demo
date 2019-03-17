17249045c1fa4421f1035a1ed1b4976e8670c3b6
TedYim <yan3579@gmail.com>
2019-03-17 17:28:16 +0800

    增加自己实现的分布式互斥锁,并提交测试Demo

---
+++ src/main/java/com/example/demo/zookeeper/MyInterProcessMutex.java
@@ -0,0 +1,83 @@
+package com.example.demo.zookeeper;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.framework.recipes.locks.Reaper;
+import org.apache.curator.utils.CloseableUtils;
+import org.apache.curator.utils.ThreadUtils;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("deprecation")
+public class MyInterProcessMutex extends InterProcessMutex {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyInterProcessMutex.class);
+
+    private Reaper reaper;
+
+    private CuratorFramework client;
+
+    private String path;
+
+    private static final Integer DEFAULT_REAPING_THRESHOLD_MS = 30000;
+
+
+    public MyInterProcessMutex(CuratorFramework client, String path) {
+        this(client, path, DEFAULT_REAPING_THRESHOLD_MS);
+    }
+
+
+    public MyInterProcessMutex(CuratorFramework client, String path, Integer reapingThresholdMs) {
+        super(client, path);
+        this.client = client;
+        this.path = path;
+        this.reaper = new Reaper(client, MyInterProcessMutexThreadPool.getInstance(),
+                (reapingThresholdMs != null && reapingThresholdMs != 0) ? reapingThresholdMs : DEFAULT_REAPING_THRESHOLD_MS, (String) null);
+        try {
+            reaper.start();
+            LOGGER.debug("reaper start ...");
+        } catch (Exception ignored) {
+        }
+    }
+
+
+    @Override
+    public void acquire() throws Exception {
+        super.acquire();
+        this.reaper.addPath(this.path, Reaper.Mode.REAP_UNTIL_DELETE);
+        LOGGER.debug("reaper add path : [ {} ]", this.path);
+    }
+
+
+    @Override
+    public void release() throws Exception {
+        super.release();
+        for (; ; ) {
+            TimeUnit.SECONDS.sleep(10);
+            Stat stat = client.checkExists().forPath(path);
+            if (stat == null) {
+                reaper.removePath(path);
+                LOGGER.debug("reaper remove path : [ {} ]", this.path);
+                CloseableUtils.closeQuietly(reaper);
+                LOGGER.debug("reaper close ...");
+                break;
+            }
+        }
+    }
+
+    static class MyInterProcessMutexThreadPool {
+
+        private MyInterProcessMutexThreadPool() {
+        }
+
+        private static final ScheduledExecutorService instance = ThreadUtils.newSingleThreadScheduledExecutor("MyReaper");
+
+        public static ScheduledExecutorService getInstance() {
+            return instance;
+        }
+    }
+}
